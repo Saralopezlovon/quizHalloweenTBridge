62e4fa0a78dae3ae7fd8aa92ea312891b1ab9e8f
Saralopezlovon <lopezlovonsara@gmail.com>
2021-10-24 19:57:45 +0200

    Add structure of SPA

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -39,19 +39,49 @@ const drawQuiz =  () => {
 
     .then (objDatos=>{
 
-    //   console.log(objDatos) 
+    //   console.log(objDatos)       
 
       document.getElementById('question').innerHTML= `${objDatos.questions[0]}`
 
        let incorrect = objDatos.incorrectAnswers[0] 
 
-      document.getElementById('option1').innerHTML=`${incorrect[0]}`        
+      document.getElementById('option1').innerHTML=incorrect[0]        
 
-      document.getElementById('option2').innerHTML = `${incorrect[1]}`
+      document.getElementById('option2').innerHTML = incorrect[1]
 
-      document.getElementById('option3').innerHTML=`${objDatos.correctAnswer[0]}`
+      document.getElementById('option3').innerHTML=objDatos.correctAnswer[0]
 
-      document.getElementById('option4').innerHTML= `${incorrect[2]}`         
+      document.getElementById('option4').innerHTML= incorrect[2]      
+
+
+      let contador = 1;
+
+      document.getElementById('btn-next').addEventListener('click', ()=>{
+
+          if (contador < 9){
+
+            contador++
+    
+            document.getElementById('question').innerHTML= objDatos.questions[contador]
+    
+            incorrect = objDatos.incorrectAnswers[contador] 
+     
+           document.getElementById('option1').innerHTML= incorrect[0]        
+     
+           document.getElementById('option2').innerHTML = incorrect[1]
+     
+           document.getElementById('option3').innerHTML= objDatos.correctAnswer[contador]
+     
+           document.getElementById('option4').innerHTML= incorrect[2]       
+    
+          }else{
+
+            console.log("Aqui debo poner la pagina de resultados")
+
+          }
+
+      })
+      
         
     }) 
 }
@@ -74,7 +104,7 @@ const validate = () => {
     document.getElementById('option3').addEventListener('click', () => {
         document.getElementById("body").style.backgroundColor = "green"
         alert ("Congratulations! This is the right one!")
-        document.getElementById("btn-next").display= "flex"
+        document.getElementById("btn-next").style.display= "flex"
 
     })
 
@@ -82,7 +112,6 @@ const validate = () => {
         document.getElementById("body").style.backgroundColor = "red"
         alert ("Incorrect answer, try again!")
     })
-
 
 }
 
