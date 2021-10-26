ed7b9ba4dc1f581d78524c2bb0224eca4b29fd7e
Saralopezlovon <lopezlovonsara@gmail.com>
2021-10-26 20:09:12 +0200

    Add SPA display

---
+++ scripts/show.js
@@ -0,0 +1,12 @@
+const pantallaLanding =  document.getElementById("body-container-login")
+const pantallaQuiz =  document.getElementById("body-container-quiz")
+
+//Cuando hacemos click en empezar se oculta pantallaLanding y se muestra pantallaQuiz
+
+document.getElementById('btn-start').addEventListener('click', ()=>{
+    console.log("Hago click")
+    pantallaLanding.style.display = "none"
+    pantallaQuiz.style.display = "flex"
+
+})
+
